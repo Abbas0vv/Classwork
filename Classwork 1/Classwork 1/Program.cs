77e549f0a3aec2469164b1068fdb206d7cd065c6
IlhamnA <IlhamnA@CARR9>
2023-03-07 21:56:11 +0400

    Completed homework with "while" operator.

--- Classwork 1/Classwork 1/Program.cs
+++ Classwork 1/Classwork 1/Program.cs
@@ -8,102 +8,114 @@ namespace Classwork_1
         {
             //----------------------------TASK_ONE-------------------------
 
+
+            Console.WriteLine("Plase write you full name : ");
+            string full_name_ = Console.ReadLine();
+
             Console.WriteLine("Amount (AZN) : ");
             double amount_azn_ = Int32.Parse(Console.ReadLine());
 
+            Console.WriteLine("How many currency will be used : ");
+            int currencyCount = int.Parse(Console.ReadLine());
 
-            if (amount_azn_ >= 25000)
+            while (currencyCount > 0)
             {
-                Console.WriteLine("There are not that many funds in the bank");
+                Console.WriteLine("Please enter the currency : ");
+                double currency = double.Parse(Console.ReadLine());
+                double aznTo = amount_azn_ / currency;
+                Console.WriteLine();
+                Console.WriteLine("Result : ");
+                Console.WriteLine(aznTo);
+                currencyCount--;
             }
-            else
-            {
-
-                if (amount_azn_ >= 1000)
-                {
-                    Console.WriteLine("Plase write you full name : ");
-                    string full_name_ = Console.ReadLine();
-
-
-                    double currencydollar = 1.70;
-                    double currencyeur = 1.81;
-                    double currencyrub = 0.023;
-
-
-
-                    double azn_to_usd_ = amount_azn_ / currencydollar;
-                    double azn_to_eur_ = amount_azn_ / currencyeur;
-                    double azn_to_rub_ = amount_azn_ / currencyrub;
-
-
-                    Console.WriteLine("United States Dollar(USD) : ");
-                    Console.WriteLine(azn_to_usd_);
-
-                    Console.WriteLine("Euro (EUR) : ");
-                    Console.WriteLine(azn_to_eur_);
-
-                    Console.WriteLine("Russian ruble (RUB) : ");
-                    Console.WriteLine(azn_to_rub_);
-                }
-                else
-                {
-                    Console.WriteLine("Sorry we can not translate that.");
-                }
-
-            }
-
 
             //----------------------------TASK_TWO-------------------------
 
             Console.WriteLine("Budget : ");
-            double budget = Int64.Parse(Console.ReadLine());
+            double budget = double.Parse(Console.ReadLine());
+            Console.WriteLine("How many Ministry do you need : ");
+            int ministryCount = int.Parse(Console.ReadLine());
 
-            double to_ministry_of_defense_ = budget * 20 / 100;
-            double to_minisrty_of_high_technologies_ = budget * 10 / 100;
-            double remaining_budget_ = budget - to_ministry_of_defense_ - to_minisrty_of_high_technologies_;
+            while (ministryCount > 0)
+            {
 
-            Console.WriteLine("To the Ministry of Defense : ");
-            Console.WriteLine(to_ministry_of_defense_);
+                Console.WriteLine("Please write you ministry : ");
+                string ministry = Console.ReadLine();
 
-            Console.WriteLine("To the Ministry of High Technologies : ");
-            Console.WriteLine(to_minisrty_of_high_technologies_);
+                Console.WriteLine("What percentage will go : ");
+                double percentage = double.Parse(Console.ReadLine());
+                double ministryBudget = budget * percentage / 100;
 
-            Console.WriteLine("Remaining budget : ");
-            Console.WriteLine(remaining_budget_);
+                Console.WriteLine(ministry + " : " + ministryBudget);
+                ministryCount--;
+                budget = budget - ministryBudget;
+            }
+            Console.WriteLine("Budget : " + budget);
 
             //----------------------------TASK_THREE-------------------------
 
-            Console.Write("Company : ");
-            string first_shoe_ = Console.ReadLine();
-            Console.Write("Number of shoes : ");
-            int first_shoe_count_ = Int32.Parse(Console.ReadLine());
-            Console.Write("Price per one ($) : ");
-            double first_shoe_price_ = Int32.Parse(Console.ReadLine());
-            Console.Write("Price of all together($) :");
-            double first_total_ = first_shoe_count_ * first_shoe_price_;
-            Console.WriteLine(first_total_);
+            Console.WriteLine("How many Company do you need : ");
+            int companyCount = int.Parse(Console.ReadLine());
+            decimal resulttotal = 0;
 
+            while (companyCount > 0)
+            {
+                Console.Write("Company : ");
+                string shoe = Console.ReadLine();
 
-            Console.Write("Company : ");
-            string second_shoe_ = Console.ReadLine();
-            Console.Write("Number of shoes : ");
-            int second_shoe_count_ = Int32.Parse(Console.ReadLine());
-            Console.Write("Price per one ($) : ");
-            double second_shoe_price_ = Int32.Parse(Console.ReadLine());
-            Console.Write("Price of all together($) :");
-            double second_total_ = second_shoe_count_ * second_shoe_price_;
-            Console.WriteLine(second_total_);
+                Console.Write("Number of shoes : ");
+                decimal shoeCount = 0;
 
+                while (true)
+                {
+                    Console.Write("Number of shoes : ");
+                    shoeCount = decimal.Parse(Console.ReadLine());
 
-            Console.Write("Company : ");
-            string third_shoe_ = Console.ReadLine();
-            Console.Write("Number of shoes : ");
-            int third_shoe_count_ = Int32.Parse(Console.ReadLine());
-            Console.Write("Price per one ($) : ");
-            double third_shoe_price_ = Int32.Parse(Console.ReadLine());
-            Console.Write("Price of all together($) :");
-            double third_total_ = third_shoe_count_ * third_shoe_price_;
-            Console.WriteLine(third_total_);
+                    if (shoeCount > 0 && shoeCount < 10)
+                    {
+                        break;
+                    }
+                }
+
+                decimal shoePrice = 0;
+
+                while (true)
+                {
+                    Console.WriteLine("Price per one ($) : ");
+                    shoePrice = decimal.Parse(Console.ReadLine());
+
+                    if (shoePrice > 50 && shoePrice < 500)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("The price should be between 50 and 500!");
+                    }
+                }
+
+                Console.Write("Total Price ($) : ");
+                decimal total = shoeCount * shoePrice;
+
+                Console.WriteLine(shoeCount * shoePrice);
+
+                Console.WriteLine();
+                Console.WriteLine();
+                companyCount--;
+                resulttotal = resulttotal + total;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Price of all together($) : ");
+            Console.WriteLine(resulttotal);
+
+            if (companyCount >= 5)
+            {
+                resulttotal = resulttotal * 15 / 100;
+            }
+            else if (companyCount >= 10)
+            {
+                resulttotal = resulttotal * 25 / 100;
+            }
         }
     }
 }
